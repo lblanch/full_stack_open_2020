012f75e763bcbf063891df25b2efbe6ce107799f
Laura Blanch <laura.blancho@gmail.com>
2021-02-06 01:07:13 +0200

    2.12: Data for countries, step1

---
+++ part2/countries/src/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Countries from './components/Countries'
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState('')
+    
+    const filteredCountries = countries.filter((country) => country.name.match(new RegExp(search,"i")))
+
+    const handleSearchChange = (event) => setSearch(event.target.value)
+    
+    useEffect(() => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all?fields=name;capital;population;languages;flag')
+            .then((response) => setCountries(response.data))
+    },[])
+
+    return (
+        <div>
+            find countries <input value={search} onChange={handleSearchChange} />
+            <Countries countries={filteredCountries} />
+        </div>
+    )
+}
+
+export default App
---
+++ part2/countries/src/components/Countries.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Country = ({country}) => (
+    <div>
+        <h2>{country.name}</h2>
+        <div>capital {country.capital}</div>
+        <div>population {country.population}</div>
+        <h3>languages</h3>
+        <ul>
+            {country.languages.map((language) => <li key={language.iso639_1}>{language.name}</li>)}
+        </ul>
+        <img src={country.flag} width='100px' />
+    </div>
+)
+
+const Countries = ({countries}) => {
+    if (countries.length === 0) {
+        return <p>No countries match the filter</p>
+    }
+
+    if (countries.length > 10) {
+        return <p>Too many matches, specify another filter</p>
+    }
+
+    if (countries.length > 1) {
+        return <div>{countries.map((country) => <p key={country.name}>{country.name}</p>)}</div>
+    }
+
+    return <Country country={countries[0]} />
+}
+
+export default Countries
