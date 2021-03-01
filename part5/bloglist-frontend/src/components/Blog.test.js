eb980b5f668ef25c4c54e439467035e42d5072d1
Laura Blanch <laura.blancho@gmail.com>
2021-03-01 18:21:28 +0200

    5.15: Blog list tests, step3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,22 +3,23 @@ import { render, fireEvent } from '@testing-library/react'
 
 import Blog from './Blog'
 
-describe('<Blog /> togglability', () => {
-    const blog = {
-        title: 'test title for test',
-        author: 'Test McAuthor',
-        url: 'www.testurl.com',
-        likes: 9,
-        user: {
-            name: 'Test McUser',
-            username: 'test-root'
-        }
+const blog = {
+    title: 'test title for test',
+    author: 'Test McAuthor',
+    url: 'www.testurl.com',
+    likes: 9,
+    user: {
+        name: 'Test McUser',
+        username: 'test-root'
     }
+}
+
+describe('<Blog /> togglability', () => {
     let component
 
     beforeEach(() => {
         component = render(
-            <Blog blog={blog} loggedUser="Test McUser" />
+            <Blog blog={blog} loggedUser="test-root" />
         )
     })
 
@@ -53,6 +54,56 @@ describe('<Blog /> togglability', () => {
         expect(url).toBeVisible()
         expect(likes).toBeVisible()
         expect(username).toBeVisible()
+    })
+})
+
+describe('<Blog /> like and delete functionality', () => {
+    let component
+
+    let mockLikeHandler
+    let mockDeleteHandler
+
+    beforeEach(() => {
+        mockLikeHandler = jest.fn()
+        mockDeleteHandler = jest.fn()
+
+        component = render(
+            <Blog
+                blog={blog}
+                likeBlog={mockLikeHandler}
+                deleteBlog={mockDeleteHandler}
+                loggedUser="test-root"
+            />
+        )
+
+        const viewButton = component.getByRole('button', { name: 'view' })
+        fireEvent.click(viewButton)
+    })
+
+    test('like button fires the like function', () => {
+        const likeButton = component.getByRole('button', { name: 'like' })
+        fireEvent.click(likeButton)
+
+        expect(mockLikeHandler.mock.calls).toHaveLength(1)
+
+        fireEvent.click(likeButton)
+        fireEvent.click(likeButton)
+        fireEvent.click(likeButton)
+
+        expect(mockLikeHandler.mock.calls).toHaveLength(4)
+    })
+
+    test('delete button fires the delete function', () => {
+        const deleteButton = component.getByRole('button', { name: 'remove' })
+        fireEvent.click(deleteButton)
+
+        expect(mockDeleteHandler.mock.calls).toHaveLength(1)
+
+        fireEvent.click(deleteButton)
+        fireEvent.click(deleteButton)
+        fireEvent.click(deleteButton)
+
+        expect(mockDeleteHandler.mock.calls).toHaveLength(4)
     })
 })
 
